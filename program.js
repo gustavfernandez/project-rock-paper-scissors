7a6407be460658767cc668468c364aafac7e7e0b
Gustavo <gusfernandez.g74@gmail.com>
2022-12-17 16:12:37 -0300

    Función playRound

--- program.js
+++ program.js
@@ -7,7 +7,46 @@ function getComputerChoice() {
     return choice
 }
 
+// Función que simula el juego
 
+function playRound(playerSelection, computerSelection) {
 
+    //Se pasa a minisculas las elecciones.
+    player = playerSelection.toLowerCase()
+    computer = computerSelection.toLowerCase()
+
+    // comparar la elección del jugador con la del computador dependiendo de cada caso.
+    // Primero reviso si son iguales.
+    
+    if (player === computer) {
+        return `It's a draw, you both pick ${player}`
+        // Ahora reviso si jugador = roca con todos los posibles elecciones del computador. LUego hago lo mismo con papel y tijera.
+        } 
+        else if (player === "rock") {
+            if (computer === "scissors") {
+                return "You Win!, Rock beats Scissors"
+            } 
+            else if (computer === "paper") {
+                return "You Lose!, Paper beats Rock"
+            }
+        } 
+        else if (player === "scissors") {
+            if (computer === "rock"){
+                return "You Lose!, Rock beats Scissors"
+            } 
+            else if (computer === "paper") {
+                return "You Win!, Paper beats Scissors"
+            }
+        } 
+        else if (player === "paper") {
+            if (computer === "rock") {
+                return "You Win!, Paper beats Rock"
+            }
+            if (computer === "scissors") {
+                return "You Lose!, Scissors beats Paper"
+            }
+        }
+    
+    }
 
 
