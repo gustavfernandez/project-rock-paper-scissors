cfe7ad2ea52a2d319f53fa0f918ead3131ee5c87
Gustavo <gusfernandez.g74@gmail.com>
2022-12-17 15:57:37 -0300

    Función getComputerChoice

--- program.js
+++ program.js
@@ -1,1 +1,13 @@
 
+// Función que decide el movimiento del pc
+function getComputerChoice() {
+    choices = ["Rock", "Paper", "Scissors"]
+    choice = choices[Math.floor(Math.random()*length(choices))]
+
+    return choice
+}
+
+
+
+
+
