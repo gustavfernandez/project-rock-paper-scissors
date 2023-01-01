f215e22af5669d4f99f112dad27524093352cfae
Gustavo <gusfernandez.g74@gmail.com>
2023-01-01 12:35:18 -0300

    Cambie la lógica de game()

--- program.js
+++ program.js
@@ -16,44 +16,52 @@ function playRound(playerSelection, computerSelection) {
     // comparar la elección del jugador con la del computador dependiendo de cada caso.
     // Primero reviso si son iguales.
     if (player === computer) {
-        return `It's a draw, you both pick ${player}`
+        return [`It's a draw, you both pick ${player}`, "draw"]
         // Ahora reviso si jugador = roca con todos los posibles elecciones del computador. LUego hago lo mismo con papel y tijera.
+        // retorno un array con el mensaje a mostrar y el ganador de la ronda.
         } 
         else if (player === "rock") {
             if (computer === "scissors") {
-                return "You Win!, Rock beats Scissors"
+                return ["You Win!, Rock beats Scissors", "me"]
             } 
             else if (computer === "paper") {
-                return "You Lose!, Paper beats Rock"
+                return ["You Lose!, Paper beats Rock", "pc"]
             }
         } 
         else if (player === "scissors") {
             if (computer === "rock"){
-                return "You Lose!, Rock beats Scissors"
+                return ["You Lose!, Rock beats Scissors", "pc"]
             } 
             else if (computer === "paper") {
-                return "You Win!, Paper beats Scissors"
+                return ["You Win!, Paper beats Scissors", "me"]
             }
         } 
         else if (player === "paper") {
             if (computer === "rock") {
-                return "You Win!, Paper beats Rock"
+                return ["You Win!, Paper beats Rock", "me"]
             }
             if (computer === "scissors") {
-                return "You Lose!, Scissors beats Paper"
+                return ["You Lose!, Scissors beats Paper", "pc"]
             }
         }
-    
     }
 
 // Por último creamos la función game() para jugar 5 veces
 function game() {
-    for (let i = 1; i <= 5; i++) {
+    myWins = 0
+    computerWins = 0
+    for (let i = 1; myWins < 5 && computerWins < 5; i++) {
         playerSelection = prompt("Rock, Paper or Scissors?")
         computerSelection = getComputerChoice()
-
         round = playRound(playerSelection, computerSelection)
-        console.log(round)
+        console.log(round[0])
+        if (round[1] === "me") {myWins++} else if (round[1] === "pc") {computerWins++}
+    }
+
+    if (myWins === 5) {
+        console.log("Easy Money...")
+    } else {
+        console.log("Pc is the champion")
     }
 }
 
